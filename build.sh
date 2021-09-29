70535cc05faa4994b1525ad043a9222153cde89a
singhsupriya2819 <59437176+singhsupriya2819@users.noreply.github.com>
2021-09-29 15:08:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ install_requirements() {
     sudo ./aws/install
 
     echo -e "\e[32mInstalling kubectl\e[0m"
-    curl -o kubectl "https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl"
+    curl -o kubectl "https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.10/2020-02-22/bin/linux/amd64/kubectl"
     chmod +x ./kubectl
     mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
     echo 'export PATH=$PATH:$HOME/bin' >> ~/.bash_profile
