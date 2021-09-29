a8b010ba0108b764d4b7b727f723922dd870a9b7
singhsupriya2819 <59437176+singhsupriya2819@users.noreply.github.com>
2021-09-29 15:32:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -79,13 +79,13 @@ install_requirements() {
     sudo ./aws/install
 
     echo -e "\e[32mInstalling kubectl\e[0m"
-    curl -o kubectl "https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.10/2020-02-22/bin/linux/amd64/kubectl"
+    curl -o kubectl "https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl"
     chmod +x ./kubectl
     mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
     echo 'export PATH=$PATH:$HOME/bin' >> ~/.bash_profile
 
     echo -e "\e[32mInstalling eksctl\e[0m"
-    curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+    curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp" | tar xz -C /tmp
     sudo mv /tmp/eksctl /usr/local/bin
 
     echo -e "\e[32mInstalling docker-compose\e[0m"
